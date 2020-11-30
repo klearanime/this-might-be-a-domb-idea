898e53b036861f094e479a18dfa8b1508835a219
Crystal K Young <maybiz@hotmail.com>
2020-11-30 14:27:17 -0500

    done

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-1
+const paragraph = document.querySelector('p')
+paragraph.style.color = 'lightBlue'
+
+const heading = document.querySelector('h1')
+heading.style.fontSize = '5em'
+
+paragraph.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+const halfOpaque = document.querySelector('#item-13')
+halfOpaque.style.opacity = '0.5'
+
+const sayHi = document.querySelector('#item-3')
+sayHi.innerText = 'I say, "Hi!"'
+
+const image = document.querySelector('img')
+image.src = 
+'http://www.tioxic.com/wp-content/uploads/trex_4.jpg'
+
+image.style.height = '300px'
+
+const newImage = document.querySelector('#img2')
+newImage.src = 
+'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.TOwPpEWShc9Dyncxilhi0gAAAA%26pid%3DApi&f=1'
+
+newImage.style.height = '300px'
+
+const li16 = document.querySelector('#item-16');
+li16.innerText = 'Won\'t get fooled again';
+
